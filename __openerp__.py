263ed00489b29254d1427bc839ee6fb9643a1f03
Madhav <madhav.agarwal@verts.co.in>
2016-02-28 08:24:04 +0530

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name':'Simdi Employee Tags',
+    'version': '1',
+    'author': 'Drishti Tech',
+    'website': 'www.drishtitech.com',
+
+    'description':""" Simdi Employee Tags
+""",
+
+'data':[
+        'employee_tag_demo.xml',
+        ],
+'installable': True,
+'auto_install': False,
+'depends':['hr'],
+}
